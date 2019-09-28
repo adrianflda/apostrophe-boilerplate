9174c791013a4e6088a2c5c4bd8b8d03422e1014
adrian moreno perez <adrian.flda@gmail.com>
2019-09-28 17:40:31 -0400

    dockerized

--- app.js
+++ app.js
@@ -13,14 +13,19 @@ var apos = require('apostrophe')({
 
     // Note: most configuration occurs in the respective
     // modules' directories. See lib/apostrophe-assets/index.js for an example.
-    
+
     // However any modules that are not present by default in Apostrophe must at
     // least have a minimal configuration here: `moduleName: {}`
 
     // If a template is not found somewhere else, serve it from the top-level
     // `views/` folder of the project
 
-    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') }
+    'apostrophe-templates': {
+      viewsFolderFallback: path.join(__dirname, 'views')
+    },
+    'apostrophe-db': {
+      uri: 'mongodb://' + process.env.MONGODB_PORT_27017_TCP_ADDR + ':' + process.env.MONGODB_PORT_27017_TCP_PORT + '/oauth2'
+    },
 
   }
 });
